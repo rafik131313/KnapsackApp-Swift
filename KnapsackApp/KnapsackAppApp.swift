fc2895fe242cddf24825f5b553e983dfa5e453fd
Rafal Stefaniak <rafalstefaniak2022@gmail.com>
2022-11-24 17:18:19 +0100

    Initial Commit

---
+++ KnapsackApp/KnapsackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnapsackAppApp.swift
+//  KnapsackApp
+//
+//  Created by Rafał on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct KnapsackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
